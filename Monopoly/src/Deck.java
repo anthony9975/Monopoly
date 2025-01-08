e5d1393b5fe886a2752564a8ef14fa76d437d7d4
Anthony Ma <anthony.mm246@gmail.com>
2025-01-07 21:51:01 -0800

    Deck: draw function implemented

--- Monopoly/src/Deck.java
+++ Monopoly/src/Deck.java
@@ -1,22 +1,49 @@
+import java.util.ArrayList;
 
 public class Deck<T> {
     
     // Member Variables
     private int numCards; // Number of cards in the deck
     private T[] cards; // Array representing the cards
-    private int[] cardNumbers; // Array representing the cards numbers
+    private ArrayList<Integer> cardNumbers; // Array representing the cards numbers
 
     // Constructor
     public Deck (int numCards, T[] cards){
+        newCards(numCards, cards);
+    }
+
+    // Accessors
+    public int getNumCards (){
+        return numCards;
+    }
+
+    public T[] getCards (){
+        return cards;
+    }
+
+    // Mutators
+    public void newCards (int numCards, T[] cards){
         // Initilizes numCards and cards to input values.
         this.numCards = numCards;
         this.cards = cards;
 
         // Initilize cardNumbers based on card count
-        cardNumbers = new int[numCards];
+        cardNumbers = new ArrayList<>();
         for (int i = 0; i < numCards; i++){
-            cardNumbers[i] = i;
+            cardNumbers.add(i);
         }
     }
 
+    // Member Functions
+    // Draws one card from the deck
+    public T draw (){
+        // Randomly choose a card number
+        int num = (int)(Math.random()*cardNumbers.size());
+        int cardNum = cardNumbers.get(num);
+
+        // Remove the choosen card num
+        cardNumbers.remove(num);
+
+        return cards[cardNum]; 
+    }
 }
